3d687feb306aa17d60fac7a3c989eb4ef9880bec
CristianDavidChinoBejar <cristianrg095@gmail.com>
2025-03-08 01:26:14 -0300

    MOD: Conection to backend

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,37 +1,61 @@
-import { useState, useEffect } from "react";
-import { signInWithGoogle, logout, auth } from "../../../firebaseConfig";
+import { signInWithGoogle, auth } from "../../../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
-const Login = ({ onLogin }) => {
+const Login = () => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); // Nuevo estado de carga
 
+  // Detectar cambios de autenticación
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false); // La carga termina cuando Firebase responde
-
+    onAuthStateChanged(auth, async (currentUser) => {
       if (currentUser) {
-        console.log("Usuario autenticado:", currentUser);
-        onLogin(); // Solo llamar onLogin si el usuario está autenticado
+        setUser(currentUser);
+        await sendUserDataToBackend(currentUser); // Enviar datos al backend
+        console.log(currentUser, ' ga 0');
+        
       }
     });
+  }, []);
 
-    return () => unsubscribe();
-  }, [onLogin]);
-
-  if (loading) {
-    return <p>Cargando...</p>; // Muestra un mensaje mientras Firebase verifica el usuario
-  }
+  // Función para enviar los datos al backend
+  const sendUserDataToBackend = async (user) => {
+    const userData = {
+      name: user.displayName,
+      email: user.email,
+      uid: user.uid,
+      profilePic: user.photoURL,
+    };
+    console.log(user, ' GAA 1');
+    
+    try {
+      const response = await fetch('https://user-ms-gmkm.onrender.com/api/user/fetchGoogleData', {
+        // http://localhost:5000/api/auth
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      console.log(response, 'ga 2');
+      
+      const text = await response.text();
+      console.log(text,' pato');
+      
+      // const data = await response.json();
+      console.log("Respuesta al backend:", response);
+    } catch (error) {
+      console.error("Error al enviar los datos al backend:", error);
+    }
+  };
 
   return (
     <div>
       {user ? (
         <div>
-          <h2>Bienvenidx, {user.displayName}</h2>
+          <h2>Bienvenido, {user.displayName}</h2>
           <img src={user.photoURL} alt="Avatar" width="50" />
           <h4>{user.email}</h4>
-          <button onClick={logout}>Cerrar sesión</button>
+          <button onClick={() => auth.signOut()}>Cerrar sesión</button>
         </div>
       ) : (
         <button onClick={signInWithGoogle}>Iniciar sesión con Google</button>
